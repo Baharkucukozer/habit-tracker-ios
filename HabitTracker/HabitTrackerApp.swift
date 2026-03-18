8fdd7096f99c3074dbc66058970091cbdbbee031
Baharkucukozer <bahar.kucukozer@sabanciuniv.edu>
2026-03-18 14:49:57 +0300

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Bahar Küçüközer on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Bahar Küçüközer on 18.03.2026.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
